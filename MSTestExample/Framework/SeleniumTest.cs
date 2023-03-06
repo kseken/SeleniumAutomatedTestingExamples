f2eb1d9ea6b63a65a2e3d710fef51128ac90d42f
kseken <41028210+kseken@users.noreply.github.com>
2023-03-05 19:26:02 -0500

    initial

---
+++ MSTestExample/Framework/SeleniumTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestExample.Framework
+{
+    [TestClass]
+    public class SeleniumTest
+    {
+        protected IWebDriver driver;
+
+        //public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void CreateDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestCleanup]
+        public void QuitDriver()
+        {
+            if (driver != null)
+                driver.Quit();
+        }
+    }
+}
---
+++ MSTestExample/Tests/Test1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using MSTestExample.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSTestExample.Tests
+{
+    [TestClass]
+    public class Test1 : SeleniumTest
+    {
+        [TestMethod]
+        public void GoToUrlAndClickButton()
+        {
+            driver.Navigate().GoToUrl("https://academic.oup.com/eurheartj/article-abstract/44/9/738/6987246?redirectedFrom=fulltext");
+
+            IWebElement AcceptCookiesButton = driver.FindElement(By.Id("accept-button"));
+            IWebElement PermissionsButton = driver.FindElement(By.Id("PermissionsLink"));
+
+            AcceptCookiesButton.Click();
+            PermissionsButton.Click();
+        }
+    }
+}
